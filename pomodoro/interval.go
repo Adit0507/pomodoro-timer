6118aece5e07aea7aa0ad2af5c736f2edd6ef5db
Adit0507 <adi05072002@gmail.com>
2025-01-04 15:47:07 +0530

    functions and methods to deal with Intervals

--- pomodoro/interval.go
+++ pomodoro/interval.go
@@ -47,14 +47,61 @@ var (
 	ErrInvalidID          = errors.New("Invalid ID")
 )
 
-// 
+// configuration required to instantiate an interval
 type IntervalConfig struct {
 	repo               Repository
-	Pomodoro           time.Duration
+	PomodoroDuration   time.Duration
 	ShortBreakDuration time.Duration
 	LongBreakDuration  time.Duration
 }
 
 func NewConfig(repo Repository, pomodoro, shortBreak, longBreak time.Duration) *IntervalConfig {
+	c := &IntervalConfig{
+		repo:               repo,
+		PomodoroDuration:   25 * time.Minute,
+		ShortBreakDuration: 5 * time.Minute,
+		LongBreakDuration:  15 * time.Minute,
+	}
 
+	if pomodoro > 0 {
+		c.PomodoroDuration = pomodoro
+	}
+	if shortBreak > 0 {
+		c.ShortBreakDuration = shortBreak
+	}
+	if longBreak > 0 {
+		c.LongBreakDuration = longBreak
+	}
+
+	return c
+}
+
+func nextCategory(r Repository) (string, error) {
+	li, err := r.Last()
+	if err != nil && err == ErrNoIntervals {
+		return CategoryPomodoro, nil
+	}
+	if err != nil {
+		return "", err
+	}
+
+	if li.Category == CategoryLongBreak || li.Category == CategoryShortBreak {
+		return CategoryPomodoro, nil
+	}
+
+	lastBreaks, err := r.Breaks(3)
+	if err != nil {
+		return "", err
+	}
+	if len(lastBreaks) < 3 {
+		return CategoryShortBreak, nil
+	}
+
+	for _, i := range lastBreaks {
+		if i.Category == CategoryLongBreak {
+			return CategoryShortBreak, nil
+		}
+	}
+
+	return CategoryLongBreak, nil
 }
