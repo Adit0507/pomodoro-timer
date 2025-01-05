42cb3cf1d0f78bb8c0a63f0f6fdc2bbfe6c3e82a
Adit0507 <adi05072002@gmail.com>
2025-01-05 18:06:14 +0530

    initialize donut widget and segment display

--- app/widgets.go
+++ app/widgets.go
@@ -3,6 +3,7 @@ package app
 import (
 	"context"
 
+	"github.com/mum4k/termdash/cell"
 	"github.com/mum4k/termdash/widgets/donut"
 	"github.com/mum4k/termdash/widgets/segmentdisplay"
 	"github.com/mum4k/termdash/widgets/text"
@@ -12,6 +13,7 @@ type widgets struct {
 	donTimer       *donut.Donut
 	disType        *segmentdisplay.SegmentDisplay
 	txtInfo        *text.Text
+	txtTimer       *text.Text
 	updateDonTimer chan []int
 	updateTxtInfo  chan string
 	updateTxtTimer chan string
@@ -37,4 +39,109 @@ func (w *widgets) update(timer []int,
 	}
 
 	redrawCh <- true
+}
+
+// initialize widgets type
+func newWidgets(ctx context.Context, errorCh chan<- error) (*widgets, error) {
+	w := &widgets{}
+	var err error
+
+	w.updateDonTimer = make(chan []int)
+	w.updateTxtType = make(chan string)
+	w.updateTxtInfo = make(chan string)
+	w.updateTxtTimer = make(chan string)
+
+	w.donTimer, err = newDonut(ctx, w.updateDonTimer, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	w.disType, err = newSegmentDisplay(ctx, w.updateTxtType, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	w.txtInfo, err = newText(ctx, w.updateTxtInfo, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	w.txtTimer, err = newText(ctx, w.updateTxtTimer, errorCh)
+	if err != nil {
+		return nil, err
+	}
+
+	return w, nil
+}
+
+func newText(ctx context.Context, updateText <-chan string, errorCh chan<- error) (*text.Text, error) {
+	txt, err := text.New()
+	if err != nil {
+		return nil, err
+	}
+
+	// listens on the given update channel for new data and updates widget with it
+	go func() {
+		for {
+			select {
+			case t := <-updateText:
+				txt.Reset()
+				errorCh <- txt.Write(t)
+
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return txt, nil
+}
+
+func newDonut(ctx context.Context, donUpdater <-chan []int, errorCh chan<- error) (*donut.Donut, error) {
+	don, err := donut.New(donut.Clockwise(), donut.CellOpts(cell.FgColor(cell.ColorBlue)))
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			select {
+			case d := <-donUpdater:
+				if d[0] <= d[1] {
+					errorCh <- don.Absolute(d[0], d[1])
+				}
+
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return don, nil
+}
+
+func newSegmentDisplay(ctx context.Context, updateText <-chan string, errorCh chan<- error) (*segmentdisplay.SegmentDisplay, error) {
+	sd, err := segmentdisplay.New()
+	if err != nil {
+		return nil, err
+	}
+
+	// update SegmentDisplay
+	go func() {
+		for {
+			select {
+			case t := <-updateText:
+				if t == "" {
+					t = " "
+				}
+				errorCh <- sd.Write([]*segmentdisplay.TextChunk{	//allows passing multiple text segments to display
+					segmentdisplay.NewChunk(t),
+				})
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return sd, nil
 }
