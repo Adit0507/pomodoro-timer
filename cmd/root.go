acdbd9fb51c9a2441b5c81bd4863f42cd47e3b12
Adit0507 <adi05072002@gmail.com>
2025-01-11 11:35:53 +0530

    workin with cobra

---
+++ cmd/root.go
@@ -0,0 +1,100 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/Adit0507/pomodoro-timer/app"
+	"github.com/Adit0507/pomodoro-timer/pomodoro"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pomo",
+	Short: "Interactive Pomodoro Timer",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repo, err := getRepo()
+		if err != nil {
+			return err
+		}
+
+		config := pomodoro.NewConfig(
+			repo,
+			viper.GetDuration("pomo"),
+			viper.GetDuration("short"),
+			viper.GetDuration("long"),
+		)
+
+		return rootAction(os.Stdout, config)
+	},
+}
+
+// reads in config file
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".pomo")
+	}
+
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file: ", viper.ConfigFileUsed())
+	}
+}
+
+func rootAction(out io.Writer, config *pomodoro.IntervalConfig) error {
+	a, err := app.New(config)
+	if err != nil {
+		return err
+	}
+
+	return a.Run()
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pomodoro-timer.yaml)")
+	rootCmd.Flags().DurationP("pomo", "p", 25*time.Minute, "Pomodoro Duration")
+	rootCmd.Flags().DurationP("short", "s", 5*time.Minute, "Short break Duration")
+	rootCmd.Flags().DurationP("long", "l", 15*time.Minute, "Long break Duration")
+
+	viper.BindPFlag("pomo", rootCmd.Flags().Lookup("pomo"))
+	viper.BindPFlag("short", rootCmd.Flags().Lookup("short"))
+	viper.BindPFlag("long", rootCmd.Flags().Lookup("long"))
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
